d2f84b40a9a5a90d0f2fcea9e2dc11fb7e9bad1d
Rob Easthope <hello@robeasthope.com>
2022-11-12 20:46:46 +0000

    Test using calc functions in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        foo: "calc(1em * 2)",
+      },
+      fontSize: {
+        foo: [
+          `calc(16px + (48 - 16) * ((100vw - 320px) / (1600 - 320)))`,
+          { lineHeight: "1" },
+        ],
+      },
+    },
   },
   plugins: [],
-}
+};
