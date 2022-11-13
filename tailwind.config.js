1acde1f3db9e1b508a931475d73144815894b634
Rob Easthope <hello@robeasthope.com>
2022-11-13 16:39:18 +0000

    Remove some test cases

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,10 @@ module.exports = {
         foo: "calc(1em * 2)",
       },
       fontSize: {
-        foo: [
-          `calc(16px + (48 - 16) * ((100vw - 320px) / (1000 - 320)))`,
-          { lineHeight: "1" },
-        ],
         clamp: [
           'clamp(16px, calc(16px + (48 - 16) * ((100vw - 320px) / (1000 - 320))), 48px)',
           { lineHeight: "1" },
         ],
-        "basic-clamp": ["clamp(200px, 40%,  400px)", { lineHeight: "1" }],
       },
     },
   },
