c64919ddfb22fa6d64e68c608052fb16895343d1
Rob Easthope <hello@robeasthope.com>
2022-11-13 19:17:38 +0000

    Test letter spacing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,16 +19,16 @@ module.exports = {
       },
       fontSize: {
         clamp: [
-          'clamp(16px, calc(16px + (48 - 16) * ((100vw - 320px) / (1000 - 320))), 48px)',
+          "clamp(16px, calc(16px + (48 - 16) * ((100vw - 320px) / (1000 - 320))), 48px)",
           { lineHeight: "1" },
         ],
-        '16-21px': [
-          'clamp(16px, calc(16px + (21 - 16) * ((100vw - 320px) / (1000 - 320))), 21px)',
+        "16-21px": [
+          "clamp(16px, calc(16px + (21 - 16) * ((100vw - 320px) / (1000 - 320))), 21px)",
           { lineHeight: "1" },
         ],
-        '24-48px': [
-          'clamp(24px, calc(24px + (48 - 24) * ((100vw - 320px) / (1000 - 320))), 48px)',
-          { lineHeight: "1" },
+        "24-48px": [
+          "clamp(24px, calc(24px + (48 - 24) * ((100vw - 320px) / (1000 - 320))), 48px)",
+          { lineHeight: "1", letterSpacing: "0.1em" },
         ],
       },
     },
