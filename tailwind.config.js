21c651d6a6e2b5edc5a5aa6ac1909cd252fa0e7f
Rob Easthope <hello@robeasthope.com>
2022-11-13 19:17:18 +0000

    Test custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       spacing: {
         foo: "calc(1em * 2)",
